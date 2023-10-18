3689b015eaaa12646c7954aa22d656c08902f2ca
ValtteriLento <valtteri.lento@gmail.com>
2023-10-18 18:31:50 +0300

    commit

--- project/js/game.js
+++ project/js/game.js
@@ -1,8 +1,7 @@
 let game;
 
 const gameOptions = {
-    playerGravity: 800,
-    playerSpeed: 300
+    playerSpeed: 200
 }
 
 window.onload = function() {
@@ -11,22 +10,107 @@ window.onload = function() {
         scale: {
             mode: Phaser.Scale.FIT,
             autoCenter: Phaser.Scale.CENTER_BOTH,
-            width: 800,
-            height: 1000
+            width: 860,
+            height: 620
         },
         pixelArt: true,
         physics: {
             default: "arcade",
             arcade: {
                 gravity: {
-                    y: 800
+                    y: 500
                 }
             }
         },
-        scene: PlayGame
+        scene: [MainMenu, PlayGame, GameOver]
     }
     game = new Phaser.Game(gameConfig);
     window.focus();
+}
+
+class MainMenu extends Phaser.Scene {
+
+    constructor() {
+        super("MainMenu")
+    }
+
+    preload() {
+        this.load.image("grassland", "assets/Grassland_BG_(288 x 208).png")
+        this.load.spritesheet("terrain", "assets/Terrain (16x16).png", {frameWidth: 16, frameHeight: 16})
+        this.load.spritesheet("kiwi", "assets/Fruits/Kiwi.png", {frameWidth: 32, frameHeight: 32})
+        this.load.spritesheet("pineapple", "assets/Fruits/Pineapple.png", {frameWidth: 32, frameHeight: 32})
+        this.load.spritesheet("collected", "assets/Fruits/Collected.png", {frameWidth: 32, frameHeight: 32})
+        this.load.spritesheet("player", "assets/Ninja Frog/Idle (32x32).png", {frameWidth: 32, frameHeight: 32})
+        this.load.spritesheet("playerJump", "assets/Ninja Frog/Jump (32x32).png", {frameWidth: 32, frameHeight: 32})
+        this.load.spritesheet("playerFall", "assets/Ninja Frog/Fall (32x32).png", {frameWidth: 32, frameHeight: 32})
+        this.load.spritesheet("playerRun", "assets/Ninja Frog/Run (32x32).png", {frameWidth: 32, frameHeight: 32})
+        this.load.audio("jump", "assets/sfx/Jump.wav")
+        this.load.audio("pickUp", "assets/sfx/PickUp.wav")
+    }
+
+    create() {
+        this.add.image(game.config.width / 2, game.config.height / 2, "grassland").setScale(3)
+
+        this.add.text(game.config.width / 2, game.config.height / 3, "Main Menu", {
+            fontSize: 48,
+            fill: "#660000"
+        }).setOrigin(0.5)
+
+        this.add.text(game.config.width / 2, game.config.height / 2, "Press SPACE to play", {
+            fontSize: 32,
+            fill: "#006600"
+        }).setOrigin(0.5)
+
+        this.input.keyboard.once("keydown-SPACE", () => {
+            this.scene.start("PlayGame")
+        })
+
+        this.anims.create({
+            key: "idle",
+            frames: this.anims.generateFrameNumbers("player", {start: 0, end: 10}),
+            frameRate: 20,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: "jump",
+            frames: [{key: "playerJump", frame: 0}],
+            frameRate: 20
+        })
+
+        this.anims.create({
+            key: "fall",
+            frames: [{key: "playerFall", frame: 0}],
+            frameRate: 20
+        })
+
+        this.anims.create({
+            key: "run",
+            frames: this.anims.generateFrameNumbers("playerRun", {start: 0, end: 5}),
+            frameRate: 20,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: "kiwi",
+            frames: this.anims.generateFrameNumbers("kiwi", {start: 0, end: 16}),
+            frameRate: 20,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: "pineapple",
+            frames: this.anims.generateFrameNumbers("pineapple", {start: 0, end: 16}),
+            frameRate: 20,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: "collected",
+            frames: this.anims.generateFrameNumbers("collected", {start: 0, end: 5}),
+            frameRate: 10
+        })
+    }
 }
 
 class PlayGame extends Phaser.Scene {
@@ -36,30 +120,8 @@ class PlayGame extends Phaser.Scene {
         this.score = 0;
     }
 
-    preload() {
-        this.load.image("background", "assets/Green.png")
-        this.load.spritesheet("terrain", "assets/Terrain (16x16).png", {frameWidth: 16, frameHeight: 16})
-        this.load.spritesheet("kiwi", "assets/Fruits/Kiwi.png", {frameWidth: 32, frameHeight: 32})
-        this.load.spritesheet("pineapple", "assets/Fruits/Pineapple.png", {frameWidth: 32, frameHeight: 32})
-        this.load.spritesheet("collected", "assets/Fruits/Collected.png", {frameWidth: 32, frameHeight: 32})
-        this.load.spritesheet("player", "assets/Ninja Frog/Idle (32x32).png", {frameWidth: 32, frameHeight: 32})
-        this.load.spritesheet("playerJump", "assets/Ninja Frog/Jump (32x32).png", {frameWidth: 32, frameHeight: 32})
-        this.load.spritesheet("playerFall", "assets/Ninja Frog/Fall (32x32).png", {frameWidth: 32, frameHeight: 32})
-        this.load.spritesheet("playerRun", "assets/Ninja Frog/Run (32x32).png", {frameWidth: 32, frameHeight: 32})
-    }
-
     create() {
-        let x = 80;
-        let y = 80;
-
-        for (let i = 0; i < 7; i++) {
-            for (let j = 0; j < 5; j++) {
-                this.add.image(x, y, "background").setScale(2.5)
-                x += 160
-            }
-            x = 80
-            y += 160
-        }
+        this.add.image(game.config.width / 2, game.config.height / 2, "grassland").setScale(3)
 
         this.terrain = this.physics.add.group({
             immovable: true,
@@ -69,8 +131,8 @@ class PlayGame extends Phaser.Scene {
         this.fruits = this.physics.add.group()
 
         for (let i = 0; i < 20; i++) {
-            const x = Phaser.Math.Between(50, 750)
-            const y = 50 * i
+            const x = Phaser.Math.Between(200, 800)
+            const y = 200 + (20 * i)
 
             this.terrain.create(x, y, "terrain", 188)
             this.terrain.create(x + 16, y, "terrain", 189)
@@ -79,95 +141,47 @@ class PlayGame extends Phaser.Scene {
             this.terrain.create(x + 64, y, "terrain", 190)
         }
 
-        this.terrain.create(368, 600, "terrain", 188)
-        this.terrain.create(384, 600, "terrain", 189)
-        this.terrain.create(400, 600, "terrain", 189)
-        this.terrain.create(416, 600, "terrain", 189)
-        this.terrain.create(432, 600, "terrain", 190)
+        this.terrain.create(68, game.config.height / 2 - 16, "terrain", 210)
+        this.terrain.create(68, game.config.height / 2, "terrain", 188)
+        this.terrain.create(84, game.config.height / 2, "terrain", 189)
+        this.terrain.create(100, game.config.height / 2, "terrain", 189)
+        this.terrain.create(116, game.config.height / 2, "terrain", 189)
+        this.terrain.create(132, game.config.height / 2, "terrain", 190)
 
-        this.player = this.physics.add.sprite(400, 560, "player")
+        this.player = this.physics.add.sprite(88, 280, "player")
         this.physics.add.collider(this.player, this.terrain)
 
-        this.player.body.checkCollision.up = false
-        this.player.body.checkCollision.left = false
-        this.player.body.checkCollision.right = false
-
         this.physics.add.collider(this.fruits, this.terrain)
-        this.physics.add.overlap(this.player, this.fruits, this.collectFruit, null, this);
+        this.physics.add.overlap(this.player, this.fruits, this.collectFruit, null, this)
 
-        this.scoreText = this.add.text(32, 10, "0", {fontSize: "30px", fill: "#000000"});
+        this.scoreText = this.add.text(32, 10, "0", {fontSize: "30px", fill: "#000000"})
 
         this.cursors = this.input.keyboard.createCursorKeys()
-
-        this.anims.create({
-            key: "idle",
-            frames: this.anims.generateFrameNumbers("player", {start: 0, end: 10}),
-            frameRate: 10,
-            repeat: -1
-        })
-
-        this.anims.create({
-            key: "jump",
-            frames: [{key: "playerJump", frame: 0}],
-            frameRate: 10
-        })
-
-        this.anims.create({
-            key: "fall",
-            frames: [{key: "playerFall", frame: 0}],
-            frameRate: 10
-        })
-
-        this.anims.create({
-            key: "run",
-            frames: this.anims.generateFrameNumbers("playerRun", {start: 0, end: 5}),
-            frameRate: 10,
-            repeat: -1
-        })
-
-        this.anims.create({
-            key: "kiwi",
-            frames: this.anims.generateFrameNumbers("kiwi", {start: 0, end: 16}),
-            frameRate: 10,
-            repeat: -1
-        })
-
-        this.anims.create({
-            key: "pineapple",
-            frames: this.anims.generateFrameNumbers("pineapple", {start: 0, end: 16}),
-            frameRate: 10,
-            repeat: -1
-        })
-
-        this.anims.create({
-            key: "collected",
-            frames: this.anims.generateFrameNumbers("collected", {start: 0, end: 5}),
-            frameRate: 10
-        })
 
         this.triggerTimer = this.time.addEvent({
             callback: this.addTerrain,
             callbackScope: this,
             delay: 1200,
             loop: true
-        });
+        })
 
     }
 
     addTerrain() {
-        const x = Phaser.Math.Between(50, 750)
-        const y = 0
+        const x = 800
+        const y = Phaser.Math.Between(200, 500)
 
         this.terrain.create(x, y, "terrain", 188)
         this.terrain.create(x + 16, y, "terrain", 189)
         this.terrain.create(x + 32, y, "terrain", 189)
         this.terrain.create(x + 48, y, "terrain", 189)
         this.terrain.create(x + 64, y, "terrain", 190)
-        this.terrain.setVelocityY(gameOptions.playerSpeed / 6);
+        this.terrain.setVelocityX(-gameOptions.playerSpeed / 5)
 
-        if(Phaser.Math.Between(0, 1)) {
-            this.fruits.create(Phaser.Math.Between(0, game.config.width), 0, "kiwi").anims.play("kiwi", true)
-            this.fruits.create(Phaser.Math.Between(0, game.config.width), 0, "pineapple").anims.play("pineapple", true)
+        if (Phaser.Math.Between(0, 1)) {
+            this.fruits.create(Phaser.Math.Between(200, game.config.width), 0, "kiwi").anims.play("kiwi", true)
+            this.fruits.create(Phaser.Math.Between(200, game.config.width), 0, "pineapple").anims.play("pineapple", true)
+            this.fruits.setVelocityX(-gameOptions.playerSpeed / 5)
         }
     }
 
@@ -176,6 +190,7 @@ class PlayGame extends Phaser.Scene {
         this.score += 1;
         this.scoreText.setText(this.score)
         fruit.disableBody(true, true)
+        this.sound.play("pickUp")
     }
 
     update() {
@@ -188,14 +203,17 @@ class PlayGame extends Phaser.Scene {
             this.player.body.velocity.x = gameOptions.playerSpeed
             this.player.setFlipX(false)
             this.player.anims.play("run", true)
-        }
-        else {
+        } else if (this.player.body.touching.down) {
+            this.player.body.velocity.x = -gameOptions.playerSpeed / 5
+            this.player.anims.play("idle", true)
+        } else {
             this.player.body.velocity.x = 0;
             this.player.anims.play("idle", true)
         }
 
         if (this.cursors.up.isDown && this.player.body.touching.down) {
-            this.player.body.velocity.y = -800 / 1.6
+            this.player.body.velocity.y = -500 / 1.6
+            this.sound.play("jump")
         }
 
         if (this.player.body.velocity.y < 0) {
@@ -204,8 +222,41 @@ class PlayGame extends Phaser.Scene {
             this.player.anims.play("fall", true)
         }
 
-        if (this.player.y > game.config.height) {
-            this.scene.start("PlayGame");
+        if (this.player.y > game.config.height || this.player.x < 0) {
+            this.scene.start("GameOver")
         }
+    }
+}
+
+class GameOver extends Phaser.Scene {
+    constructor() {
+        super("GameOver")
+    }
+
+    create() {
+        this.add.image(game.config.width / 2, game.config.height / 2, "grassland").setScale(3)
+
+        this.add.text(game.config.width / 2, game.config.height / 3, "Game Over", {
+            fontSize: 48,
+            fill: "#ff0000"
+        }).setOrigin(0.5)
+
+        this.add.text(game.config.width / 2, game.config.height / 2, "Press SPACE to play again", {
+            fontSize: 32,
+            fill: "#006600"
+        }).setOrigin(0.5)
+
+        this.add.text(game.config.width / 2, game.config.height / 1.75, "or ESC to go to Main Menu", {
+            fontSize: 32,
+            fill: "#006600"
+        }).setOrigin(0.5)
+
+        this.input.keyboard.once("keydown-SPACE", () => {
+            this.scene.start("PlayGame")
+        })
+
+        this.input.keyboard.once("keydown-ESC", () => {
+            this.scene.start("MainMenu")
+        })
     }
 }
